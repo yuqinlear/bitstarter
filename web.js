b140730cf172d3f9c38b1d62a0abb1b96d9e343e
Changpeng <lee_leopold@hotmail.com>
2013-07-11 23:24:48 -0400

    not used

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var str = fs.readFileSync('bitstarter/index.html','utf-8');
+var str = fs.readFileSync('https://github.com/yuqinlear/bitstarter/blob/27a60e0f1cf672b960ae4686abd072b5e0a38a9c/index.html','utf-8');
 
 app.get('/', function(request, response) {
   response.send(str);
