2eb1c3c933cb0c5a1c1880728777f3c1eb2cd81e
Changpeng <lee_leopold@hotmail.com>
2013-07-11 22:36:59 -0400

    remove buffer

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(fs.readFileSync('index.html','utf-8'));
+var str = fs.readFileSync('bitstarter/index.html','utf-8');
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString);
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
